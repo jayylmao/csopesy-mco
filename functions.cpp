7b3fdf05a86cc4885dab8b927064ad5c59354cbf
jayylmao <73204320+jayylmao@users.noreply.github.com>
2025-05-11 19:34:48 +0800

    feat: initial commit

---
+++ functions.cpp
@@ -0,0 +1,15 @@
+//
+// Created by jaymi on 11/05/2025.
+//
+
+#include "functions.h"
+
+#include <iostream>
+
+void printHeader()
+{
+    std::cout << " _______ ___  ___  ___ ___ __ __\n"
+                 "/ __(_-</ _ \\/ _ \\/ -_|_-</ // /\n"
+                 "\\__/___/\\___/ .__/\\__/___/\\_, / \n"
+                 "           /_/           /___/  \n";
+}
---
+++ functions.h
@@ -0,0 +1,12 @@
+//
+// Created by jaymi on 11/05/2025.
+//
+
+#ifndef FUNCTIONS_H
+#define FUNCTIONS_H
+
+#endif //FUNCTIONS_H
+/**
+ * @brief Print the header text introducing the user to the command line.
+ */
+void printHeader();
