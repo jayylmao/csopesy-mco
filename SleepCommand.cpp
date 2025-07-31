8bd1df2142b97755322629a7a944a6ae6226f31a
belzooper78 <karldiegokarldiego@gmail.com>
2025-07-31 09:01:12 +0800

    Fix: sleepcommand had std::chrono twice

--- SleepCommand.cpp
+++ SleepCommand.cpp
@@ -8,7 +8,7 @@ void SleepCommand::execute(Process& process) {
    /*std::cout << "PID: " << process.getPID()
         << " sleeping for " << static_cast<int>(ticks) << " ticks..." << std::endl;*/
     int delay = std::max(1, static_cast<int>(ticks)) * std::max(1, static_cast<int>(ICommand::PER_INSTRUCTION_DELAY_MS));//ensures they dont multiply to 0
-    std::this_thread::sleep_for(std::chrono::milliseconds(std::chrono::milliseconds(delay)));
+    std::this_thread::sleep_for(std::chrono::milliseconds(delay));
 }
 
 
